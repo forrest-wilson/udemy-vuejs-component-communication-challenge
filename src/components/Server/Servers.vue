23241d4dcf2218492929ad5813e3ad94a9e21097
Forrest Wilson <forrest.wilson@me.com>
2018-04-20 21:00:19 +1200

    what the fuck

--- src/components/Server/Servers.vue
+++ src/components/Server/Servers.vue
@@ -1,16 +1,33 @@
 <template>
     <div class="col-xs-12 col-sm-6">
         <ul class="list-group">
-            <li
-                    class="list-group-item"
-                    v-for="index in 5">
-                Server #{{ index }}
-            </li>
+            <appServer 
+                v-for="server in servers"
+                :key="server.id"
+                :server="server">
+            </appServer>
         </ul>
     </div>
 </template>
 
 <script>
+    import Server from "./Server.vue";
+
+    export default {
+        data: function() {
+            return {
+                servers: [
+                    { id: 1, status: "Normal" },
+                    { id: 2, status: "Critical" },
+                    { id: 3, status: "Unknown" },
+                    { id: 4, status: "Normal" }
+                ]
+            };
+        },
+        components: {
+            appServer: Server
+        }
+    }
 </script>
 
 <style>
